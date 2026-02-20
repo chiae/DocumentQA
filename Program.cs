2cc506095c5c2360aa506d3a26978c2d2c2e9cf1
Emanuel Chiaburu <emanchiaburu@hotmail.com>
2026-02-20 14:00:44 -0500

    Bind Kestrel to Render PORT

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ using OpenAI;
 
 
 var builder = WebApplication.CreateBuilder(args);
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("DevCors", policy =>
